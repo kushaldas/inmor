db1e3a62d7fe5116846f51a97a01b9c28ca158cb
Kushal Das <kushal@sunet.se>
2025-04-27 10:40:21 +0200

    Adds initial python tooling as helper tools

---
+++ src/pyinmor/__init__.py
@@ -0,0 +1,64 @@
+import json
+import sys
+from datetime import datetime
+from importlib.metadata import metadata
+
+import httpx
+import redis
+from jwcrypto import jwk, jwt
+
+
+def add_subordinate(entity_id: str) :
+    """Adds a new subordinate to the federation.
+
+    :args entity_id: The entity_id to be added
+    """
+    resp = httpx.get(f"{entity_id}/.well-known/openid-federation")
+    text = resp.text
+    jwt_net = jwt.JWT.from_jose_token(text)
+    # FIXME: In future we will need the proper key to verify the signature and use only
+    # validated contain.
+    payload = json.loads(jwt_net.token.objects.get("payload").decode("utf-8"))
+    
+    # TODO: Verify that the authority_hints matches with the inmor's entity_id.
+
+
+    # This is the data we care for now
+    sub_data = {"iss": "http://localhost:8080"}
+    sub_data["jwks"] = payload.get("jwks")
+    sub_data["authority_hints"] = payload.get("authority_hints")
+    metadata = payload.get("metadata")
+    if metadata:
+        sub_data["metadata"] = metadata
+    metadata_policy = payload.get("metadata_policy")
+    if metadata_policy:
+        sub_data["metadata_policy "]= metadata_policy
+    trust_marks = payload.get("trust_marks")
+    if trust_marks:
+        sub_data["trust_marks"] = trust_marks
+    sub_data["exp"] = payload.get("exp")
+    sub_data["sub"]= payload.get("sub")
+    # creation time
+    sub_data["iat"] = datetime.now().timestamp()
+
+    # TODO: later we will have to get the key from a configuration file/location
+    private_key_data = open("./private.json").read()
+    key = jwk.JWK.from_json(private_key_data)
+
+    # TODO: fix the alg value for other types of kyes
+    token = jwt.JWT(header={"alg": "RS256", "kid": key.kid}, claims=sub_data)
+    token.make_signed_token(key)
+    token_data = token.serialize()
+    # Now we should set it in the redis
+    # TODO: Use configuration to find the correct redis instance
+    r = redis.Redis()
+    r.hset("inmor:subordinates", sub_data["sub"], token_data)
+    print(f"Successfully added {entity_id}")
+
+
+
+
+
+def add_sub_main():
+    entity_id = sys.argv[1]
+    add_subordinate(entity_id)
