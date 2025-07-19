aabca8dc3c27472d69de1f12f5dafd30ca2fd5d9
Farhaan Bukhsh <farhaan.bukhsh@gmail.com>
2025-07-19 17:38:25 +0200

    fix: Changes the choice field to ModelChoiceField
    Signed-off-by: Farhaan Bukhsh <farhaan.bukhsh@gmail.com>

--- admin/trustmarks/forms.py
+++ admin/trustmarks/forms.py
@@ -2,21 +2,7 @@ from django import forms
 
 from .models import TrustMarkType
 
-try:
+class TrustMarkForm(forms.Form):
+    entity = forms.CharField(label="entity")
+    tmt_select = forms.ModelChoiceField(queryset=TrustMarkType.objects.all())
 
-    class TrustMarkForm(forms.Form):
-        entity = forms.CharField(label="entity")
-        tmt_select = forms.ChoiceField(
-            choices=[(x.id, x.tmtype) for x in TrustMarkType.objects.all()]
-        )
-
-except:  # Stupid HACK
-
-    class TrustMarkForm(forms.Form):
-        entity = forms.CharField(label="entity")
-        # HACK: till we find a better solution
-        tmt_select = forms.ChoiceField(
-            choices=[
-                ("0", "0"),
-            ]
-        )
