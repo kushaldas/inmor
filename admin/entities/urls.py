305a8e5f4ef26cc1fa594314d7cae4a10d07293a
Kushal Das <kushal@sunet.se>
2025-07-23 19:57:35 +0200

    Updates HTML template for links
    Also updates urls.py for related names.

--- admin/entities/urls.py
+++ admin/entities/urls.py
@@ -6,6 +6,6 @@ app_name = "entities"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("add/", views.add_subordinate_entity, name="add_subordinate_entity"),
-    path("list/", views.list_subordinates, name="list_subordinates"),
+    path("add/", views.add_subordinate_entity, name="add"),
+    path("list/", views.list_subordinates, name="list"),
 ]
