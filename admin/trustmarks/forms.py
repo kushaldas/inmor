f6666937c41b3e3dae6fb7bd074f8d8bc687e8e5
Valerio Lomanto <gv.lomanto@gmail.com>
2025-07-31 15:12:17 +0200

    feat: add trustmark type from interface

--- admin/trustmarks/forms.py
+++ admin/trustmarks/forms.py
@@ -6,3 +6,7 @@ from .models import TrustMarkType
 class TrustMarkForm(forms.Form):
     entity = forms.CharField(label="entity")
     tmt_select = forms.ModelChoiceField(queryset=TrustMarkType.objects.all())
+
+
+class TrustMarkTypeForm(forms.Form):
+    type = forms.CharField(label="trust mark type")
--- admin/trustmarks/views.py
+++ admin/trustmarks/views.py
@@ -6,8 +6,8 @@ from django_redis import get_redis_connection
 
 from redis import Redis
 
-from .forms import TrustMarkForm
-from .lib import TrustMarkRequest, add_trustmark
+from .forms import TrustMarkForm, TrustMarkTypeForm
+from .lib import TrustMarkRequest, TrustMarkTypeRequest, add_trustmark
 from .models import TrustMark, TrustMarkType
 
 # Create your views here.
@@ -57,4 +57,43 @@ def addtrustmark(request: HttpRequest) -> HttpResponse:
         request,
         "trustmarks/add.html",
         {"form": form, "trustmarktypes": tmts, "msg": msg},
+    )
+
+
+def type_index(request: HttpRequest):
+    return render(request, "trustmark_types/index.html")
+
+
+def list_trustmark_types(request: HttpRequest):
+    trust_mark_types_list = TrustMarkType.objects.all()
+    paginator = Paginator(trust_mark_types_list, 3)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(request, "trustmark_types/list.html", {"page_obj": page_obj})
+
+
+def add_trustmark_type(request: HttpRequest) -> HttpResponse:
+    msg = ""
+
+    if request.method == "POST":
+        form = TrustMarkTypeForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            tmr = TrustMarkTypeRequest(type=form.data["type"])
+            trust_mark_type, created = TrustMarkType.objects.get_or_create(tmtype=tmr.type)
+            if created:
+                msg = f"Added {tmr.type}"
+            else:
+                msg = f"Trust mark type {tmr.type} was already present"
+        else:
+            print("invalid form")
+            msg = "Form validation failed."
+    else:
+        form = TrustMarkTypeForm(request.POST)
+
+    return render(
+        request,
+        "trustmark_types/add.html",
+        {"form": form, "msg": msg},
     )
